50044c2165d8f3827c125177c07445aae8cd0a69
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2023-11-16 22:23:07 +0100

    last version

--- index.php
+++ index.php
@@ -1,29 +1,43 @@
 <?php
     $nomError = $prenomError = $emailError = $teleError = $msgError = "";
     $nom = $prenom = $email = $tele = $msg = "";
+    $isSucces = false;
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $prenom = verifyInput($_POST["prenom"]);
         $nom = verifyInput($_POST["nom"]);
         $email = verifyInput($_POST["email"]);
         $tele = verifyInput($_POST["tele"]);
         $msg = verifyInput($_POST["msg"]);
+        $isSucces = true;
 
         if(empty($prenom)){
             $prenomError = "*Merci de saisir votre prenom";
+            $isSucces = false;
         }
         if(empty($nom)){
             $nomError = "*Merci de saisir votre nom";
+            $isSucces = false;
         }
-        if(empty($email)){
+        if(!isValidEmail($email)){
             $emailError = "*Merci de saisir votre email";
+            $isSucces = false;
         }
-        if(empty($tele)){
+        if(!isValidPhone($tele)){
             $teleError = "*Merci de saisir votre tele";
+            $isSucces = false;
         }
         if(empty($msg)){
             $msgError = "*Merci de saisir votre msg";
+            $isSucces = false;
         }
-        
+    }
+
+    function isValidPhone($var){
+        return preg_match("/^[0-9]*$/", $var);
+    }
+
+    function isValidEmail($var){
+        return filter_var($var, FILTER_VALIDATE_EMAIL);
     }
 
     function verifyInput($var){
@@ -51,31 +65,31 @@
                     <div class="col-md-6  my-1">
                         <label for="prenom" class="form-label">Prenoms<span class="etoile">*</span></label>
                         <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Votre prenom">
-                        <p class="red-comment my-2"><?php echo $prenomError; ?></p>
+                        <p class="red-comment my-1"><?php echo $prenomError; ?></p>
                     </div>
                     <div class="col-md-6  my-1">
                         <label for="nom" class="form-label">Nom<span class="etoile">*</span></label>
                         <input type="text" class="form-control" id="nom" name="nom" placeholder="Votre nom">
-                        <p class="red-comment my-2"><?php echo $nomError; ?></p>
+                        <p class="red-comment my-1"><?php echo $nomError; ?></p>
                     </div>
                     <div class="col-md-6  my-1">
                         <label for="email" class="form-label">Email<span class="etoile">*</span></label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="abc@xyz.com">
-                        <p class="red-comment my-2"><?php echo $emailError; ?></p>
+                        <p class="red-comment my-1"><?php echo $emailError ; ?></p>
                     </div>
                     <div class="col-md-6  my-1">
                         <label for="tele" class="form-label">Telephone<span class="etoile">*</span></label>
                         <input type="tel" class="form-control" id="tele" name="tele" placeholder="Votre telephone">
-                        <p class="red-comment my-2"><?php echo $teleError; ?></p>
+                        <p class="red-comment my-1"><?php echo $teleError; ?></p>
                     </div>
                     <div class="col my-1">
                         <label for="msg" class="form-label">Message<span class="etoile">*</span></label>
                         <textarea class="form-control" id="msg" name="msg" rows="3" placeholder="Votre message"></textarea>
-                        <p class="comment my-4"><?php echo $msgError; ?></p>
+                        <p class="red-comment my-1"><?php echo $msgError; ?></p>
                     </div>
                     <div class="col-12">
                         <input class="btn btn-warning btn-submit" type="submit" value="ENVOYER">
-                        <p class="tnx">Votre mesage a ete bien envoye.M'erci de m'avoir contacter :) </p>
+                        <p class="tnx" style="display:<?php if($isSucces) echo "block"; else echo "none"; ?>;">Votre mesage a ete bien envoye.M'erci de m'avoir contacter :) </p>
                     </div>
                 </div>
             </form>
